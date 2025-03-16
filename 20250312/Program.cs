524a3dabda32190b49f6ffd9876b2d96cf99f1c2
chrismgbhs <116180951+chrismgbhs@users.noreply.github.com>
2025-03-16 21:39:40 +0800

    Update Program.cs

--- 20250312/Program.cs
+++ 20250312/Program.cs
@@ -19,22 +19,32 @@ namespace _20250312
             bool matchNum;
             bool valueChecking;
             Random random = new Random();
-
-            for (int counter = 0; counter < arrayInput.Length; counter++) 
+            
+            Console.WriteLine();
+            for (int counter = 0; counter < arrayInput.Length; counter++)
             {
                 Console.Write($"Quick pick #{counter + 1}: ");
                 int.TryParse(Console.ReadLine(), out arrayInput[counter]);
-                message = "";
-
-                if (arrayInput[counter] == 0) 
+                message = "Valid input. \n";
+            
+                for (int counter2 = 0; counter2 < counter; counter2++)
                 {
-                    counter--;
-                    message = "Invalid input.";
+                    if (arrayInput[counter2] == arrayInput[counter])
+                    {
+                        message = "Invalid input. A number has been proved to be matching with a previous number.\n";
+                        counter--;
+                    }
                 }
-
+            
+                if (arrayInput[counter] < 1 || arrayInput[counter] > 49)
+                {
+                    message = "Invalid input. Number must be between 1 and 49. \n";
+                    counter--;
+                }
+            
                 Console.WriteLine(message);
             }
-
+            
             for (int counter = 0; counter < lottoNums.Length; counter++)
             {
                 valueChecking = false;
@@ -42,7 +52,7 @@ namespace _20250312
                 {
                     genNum = random.Next(1, 50);
                     matchNum = false;
-
+            
                     for (int counter2 = 0; counter2 < lottoNums.Length; counter2++)
                     {
                         if (genNum == lottoNums[counter2])
@@ -50,7 +60,7 @@ namespace _20250312
                             matchNum = true;
                         }
                     }
-
+            
                     if (matchNum == false)
                     {
                         lottoNums[counter] = genNum;
@@ -58,48 +68,53 @@ namespace _20250312
                     }
                 }
             }
-
+            
             Console.WriteLine($"LOTTO WINNING NUMBERS");
             for (int counter = 0; counter < lottoNums.Length; ++counter)
-            { 
+            {
                 Console.Write($"{lottoNums[counter]} \t");
             }
-
-            for (int counter = 0; counter < lottoNums.Length; ++counter) 
-            { 
-                for (int inputCounter = 0; inputCounter < arrayInput.Length;)
+            
+            Console.WriteLine();
+            Console.WriteLine();
+            for (int counter = 0; counter < lottoNums.Length; ++counter)
+            {
+                for (int inputCounter = 0; inputCounter < arrayInput.Length; inputCounter++)
                 {
-                    if (arrayInput[counter] == lottoNums[counter])
+                    if (lottoNums[counter] == arrayInput[inputCounter])
                     {
-                        Console.WriteLine($"{arrayInput[counter]} matches with {lottoNums[counter]}");
+                        Console.WriteLine($"Your {arrayInput[inputCounter]} matches with {lottoNums[counter]}.");
                         matchCounter++;
                     }
                 }
             }
-
+            
+            Console.WriteLine("You have " + matchCounter + " match/es.");
+            
+            Console.WriteLine();
             if (matchCounter == 6)
             {
                 Console.WriteLine($"CONGRATULATIONS, you won Php 50, 413, 109.80");
             }
-
+            
             else if (matchCounter == 5)
             {
                 Console.WriteLine($"CONGRATULATIONS, you won Php 50, 000");
             }
-
+            
             else if (matchCounter == 4)
             {
                 Console.WriteLine($"CONGRATULATIONS, you won Php 1, 200");
             }
-
+            
             else if (matchCounter == 3)
             {
                 Console.WriteLine($"CONGRATULATIONS, you won Php 50");
             }
-
+            
             else
             {
-                Console.WriteLine("Better luck next time.");
+                Console.WriteLine("Better luck next time. Sorry, you didn't won.");
             }
             
         }
